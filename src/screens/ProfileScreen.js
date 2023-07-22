73d143522d4ce0432fc8de9cfbae9de195e9a63c
Prajeet Basnet <prajitbasnet2000@gmail.com>
2023-07-22 23:03:05 +1000

    added more features in profile screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -9,19 +9,21 @@ const ProfileScreen = () => {
 
     const user = useSelector(state => state.user.user)
   return (
-    <div className='ProfileScreen'>
+    <div className='profileScreen'>
     <Nav/>
     <div className="profileScreen_body">
         <h1>Edit Profile</h1>
         <div className="profileScreen_info">
             <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Profile_Screen" />
-        </div>
+        
         <div className="profileScreen_details">
             <h2>{user.email}</h2>
             <div className="profileScreen_plans">
+                <h3>Plans</h3>
 
                 <button onClick={() => {auth.signOut()}} className='profileScreen_signOut'>Sign Out</button>
             </div>
+        </div>
         </div>
     </div>
     </div>
